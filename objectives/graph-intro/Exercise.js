d8d12259d4cf24af2ff49e3914a9f16da034cefc
Donnie Reese <donniereese@gmail.com>
2018-01-16 15:10:00 -0600

    Finished intro graph drawings

---
+++ objectives/graph-intro/Exercise.js
@@ -0,0 +1,13 @@
+# Draw examples of the following:
+
+1. Undirected graph of 4 verts.
+![Dicrected graph of 5 verts](./img/Graph-Undirectional.png)
+
+2. Directed graph of 5 verts.
+![Directed graph of 5 verts](./img/Graph-Directional.png)
+
+3. Cyclic directed graph of 6 verts.
+![Cyclic directed graph of 6 verts](./img/Graph-Directional-Cyclic.png)
+
+4. DAG of 7 verts.
+![DAG of 7 verts](./img/Graph-DAG.png)
